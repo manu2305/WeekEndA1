f5f1c5c357ff583469fa77a41e028eeaa2667a27
Admin <Admin@DESKTOP-J8NKUK4>
2026-01-18 13:40:23 +0530

    final

---
+++ src/test/java/AdminModule/MyFinalScript.java
@@ -0,0 +1,11 @@
+package AdminModule;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class MyFinalScript {
+@Test
+public void main1() {
+	Reporter.log("Tata Bye Bye",true);
+}
+}
